f517f47105aed62e11670cd168678f1c6b55bf24
rzchen <jeff820523@gmail.com>
2020-08-31 20:45:11 +0800

    update workspace with shoulda-matches

--- spec/factories/workspace.rb
+++ spec/factories/workspace.rb
@@ -1,10 +1,5 @@
 FactoryBot.define do
   factory :workspace do
-
-    name { Faker::Name.name }
-
-    factory :workspace_with_channels do
-      channels { [association(:channel)] }
-    end
+    # name { Faker::Name.name }
   end
 end
--- spec/models/workspace_spec.rb
+++ spec/models/workspace_spec.rb
@@ -1,37 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Workspace, type: :model do
-  # 確認所有欄位的 type 是否正確 ex. name 是string
-  it "is valid with valid attributes" do
-    workspace = Workspace.new
-    workspace.name = "Anything"
-    workspace.deleted_at = DateTime.now
-    expect(workspace).to be_valid
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_uniqueness_of(:name) }
   end
 
-  # 確認 name 這個欄位為「必填」
-  # it "is not valid without a name" do
-  #   workspace = Workspace.new(name: nil)
-  #   expect(workspace).not_to be_valid
-  # end
-
-  it { should validate_presence_of(:name) }
-  
-  # 確認 name 必須要是「唯一值」
-  it "is not valid with same name" do
-    workspace1 = Workspace.new
-    workspace1.name = "WS_name"
-    workspace1.save
-    workspace2 = Workspace.new
-    workspace2.name = "WS_name"
-    expect(workspace2).not_to be_valid
+  describe 'association' do
+    it { should have_many(:channels) }
   end
 
-  # create 一個 workspace、二個 channel，測試 workspace.channels 是否包含兩個 channel
-  it "has_many channels" do
-    workspace = Workspace.create(name: "W_1")
-    channel1 = Channel.create(name: "C_1", public: true, members: 1, workspace_id: workspace.id)
-    channel2 = Channel.create(name: "C_2", public: true, members: 1, workspace_id: workspace.id)
-    expect(workspace.channels).to include(channel1, channel2)
-  end
 end
