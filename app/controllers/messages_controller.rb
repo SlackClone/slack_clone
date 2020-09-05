873d18263cdc9f051e65bfc990adebf303f943d1
rzchen <jeff820523@gmail.com>
2020-09-05 18:44:22 +0800

    add message create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,20 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
 
+  def create
+    
+    @channel = Channel.find(params[:channel_id])
+    @message = @channel.messages.new(message_params)
+    # debugger
+    if @message.save
+      redirect_to request.referrer, notice: "新增留言成功"
+    else
+
+  end  
+
+
+  private
+  def message_params
+    params.require(:message).permit(:content).merge(user: current_user)
+  end
 end
