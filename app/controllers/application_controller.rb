8bd5f433ad63a09b9b0428176ff3b20b1ae6a205
Leoyang0717 <pithend58639@gmail.com>
2020-09-07 18:18:43 +0800

    更改寫法，將判斷移至registrations_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
-
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
-    end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,19 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
   private
 
   def after_inactive_sign_up_path_for(resource)
     super(resource)
     pages_path
   end
-
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
+  
+  # If you have extra params to permit, append them to the sanitizer.
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  end
 end
