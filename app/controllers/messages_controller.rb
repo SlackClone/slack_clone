0ce2c83216c1c3d65c674328d50f2a2ca1f5ada4
rzchen <jeff820523@gmail.com>
2020-09-07 09:56:15 +0800

    fix message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,6 +15,6 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:content).merge(user: current_user.id)
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
