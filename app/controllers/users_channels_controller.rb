bab41088fa8e92553f07d6b335a394c5b0eaafdb
hank <hank8451@gmail.com>
2020-09-09 17:50:03 +0800

    fix the join channel stimulus code

--- app/controllers/users_channels_controller.rb
+++ app/controllers/users_channels_controller.rb
@@ -1,20 +1,11 @@
 class UsersChannelsController < ApplicationController
   before_action :find_channel
   def create
-    respond_to do |format|
-      format.json { render json: { status: (@channel.users.include?(current_user) ? true:false)} }
-    end
     @channel.users_channels.where(user_id: current_user.id).first_or_create
-    @workspace = Workspace.find(@channel.workspace_id)
   end
 
-
   def destroy
-    respond_to do |format|
-      format.json { render json: { status: (@channel.users.include?(current_user) ? true:false)} }
-    end
     @channel.users_channels.where(user_id: current_user.id).destroy_all
-    @workspace = Workspace.find(@channel.workspace_id)
   end
 
   private
--- app/javascript/controllers/channels_controller.js
+++ app/javascript/controllers/channels_controller.js
@@ -1,49 +1,18 @@
 import { Controller } from "stimulus"
-import Rails from "@rails/ujs";
 
 export default class extends Controller {
   static targets = [ 'symbol' ]
   
   addChannel(){
-    let channel_id = this.element.getAttribute("data-channels-id")
-    Rails.ajax({
-      url: `/channels/${channel_id}/users_channels.json`,
-      type: 'post',
-      success: (result) => {
-        let element = this.symbolTarget
-        console.log(element)
-        if (result["status"]){
-          element.textContent = "+"
-        } else {
-          element.textContent = "-"
-          element.setAttribute("data-action", "click->channels#removeChannel")
-          element.setAttribute("data-method", "delete")
-        }
-      },
-      error: (err) => {
-        console.log(err);
-      }
-    })
+    let element = this.symbolTarget
+    element.textContent = "-"
+    element.setAttribute("data-action", "click->channels#removeChannel")
+    element.setAttribute("data-method", "delete")
   }
   removeChannel(){
-    let channel_id = this.element.getAttribute("data-channels-id")
-    Rails.ajax({
-      url: `/channels/${channel_id}/users_channels.json`,
-      type: 'delete',
-      success: (result) => {
-        let element = this.symbolTarget
-        console.log(element)
-        if (result["status"]){
-          element.textContent = "+"
-          element.setAttribute("data-action", "click->channels#addChannel")
-          element.setAttribute("data-method", "post")
-        } else {
-          element.textContent = "-"
-        }
-      },
-      error: (err) => {
-        console.log(err);
-      }
-    })
+    let element = this.symbolTarget
+    element.textContent = "+"
+    element.setAttribute("data-action", "click->channels#addChannel")
+    element.setAttribute("data-method", "post")
   }
 }
