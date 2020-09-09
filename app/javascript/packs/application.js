2d5bf0fc7960e73969db84ada4cd54531dc50691
serenanamee <serena.chang169@gmail.com>
2020-09-09 13:36:12 +0800

    profile dropdown

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,12 @@ window.jQuery = $
 window.$ = $
 
 
-
+$(document).ready(function () {
+  $('.clickopen').click(function (event) {
+    event.preventDefault();
+    $('.open').slideToggle();
+  });
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
