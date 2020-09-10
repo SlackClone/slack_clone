8874391f71048f98c1378ea3ce24ec485596d634
hank <hank8451@gmail.com>
2020-09-10 17:32:12 +0800

    using @rails.ujs to resolve the javascript problem and place defer true

--- app/javascript/controllers/channels_controller.js
+++ app/javascript/controllers/channels_controller.js
@@ -1,18 +1,39 @@
 import { Controller } from "stimulus"
+import Rails from "@rails/ujs";
 
 export default class extends Controller {
   static targets = [ 'symbol' ]
   
   addChannel(){
     let element = this.symbolTarget
-    element.textContent = "-"
-    element.setAttribute("data-action", "click->channels#removeChannel")
-    element.setAttribute("data-method", "delete")
+    let channel_id = this.element.getAttribute("data-channels-id")
+    Rails.ajax({
+      url: `/channels/${channel_id}/users_channels.json`,
+      type: 'post',
+      success: () => {
+        element.textContent = "-"
+        element.setAttribute("data-action", "click->channels#removeChannel")
+        element.setAttribute("data-method", "delete")
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
   }
   removeChannel(){
     let element = this.symbolTarget
-    element.textContent = "+"
-    element.setAttribute("data-action", "click->channels#addChannel")
-    element.setAttribute("data-method", "post")
+    let channel_id = this.element.getAttribute("data-channels-id")
+    Rails.ajax({
+      url: `/channels/${channel_id}/users_channels.json`,
+      type: 'delete',
+      success: () => {
+        element.textContent = "+"
+        element.setAttribute("data-action", "click->channels#addChannel")
+        element.setAttribute("data-method", "post")
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
   }
 }
