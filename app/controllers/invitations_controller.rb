9118e669dd68d5855e46fa1eb6efe49ff27dab09
Leoyang0717 <pithend58639@gmail.com>
2020-09-10 19:38:32 +0800

    Changed invitation_controller create_action & added notice in layout/application.html.rb

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,23 +2,25 @@ class InvitationsController < ApplicationController
   before_action :authenticate_user!,:find_workspace
   def create
     @invitation = Invitation.new(invitation_params)
-    @invitation.user = current_user
-    @invitation.workspace = @workspace
-    @invitation.invitation_token = Devise.friendly_token[0,20]
+    @invitation.assign_attributes(
+      user: current_user,
+      workspace: @workspace,
+      invitation_token: Devise.friendly_token[0,20]
+    )
     if @invitation.save
-    MyMailer.invite(@invitation).deliver_now
-    redirect_to workspaces_path, notice: "邀請成功"
-  else
-    puts @invitation.errors.full_messages
-    puts "something goes wrong"
+      MyMailer.invite(@invitation).deliver_now
+      redirect_to workspaces_path, notice: "邀請成功"
+    else
+      puts @invitation.errors.full_messages
+      puts "something goes wrong"
     end
   end
 
   def accept
-    if find_invitation_token && current_user.email == find_receiver_email
+    if find_invitation && current_user.email == find_receiver_email
       if @workspace.users.find_by(email: find_receiver_email) == nil
         @workspace.users << User.find_by(email: find_receiver_email)
-        find_invitation_token.touch(:accept_at)
+        find_invitation.touch(:accept_at)
         redirect_to @workspace,notice: "歡迎#{find_receiver_email}"
       end
     else
@@ -40,7 +42,7 @@ private
     Invitation.find_by(invitation_token: params[:token]).receiver_email
   end
 
-  def find_invitation_token
+  def find_invitation
     Invitation.find_by(invitation_token: params[:token])
   end
 end
