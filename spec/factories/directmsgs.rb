c2a6aa52bbc532890dcaa01a0ce302af89d55a34
rzchen <jeff820523@gmail.com>
2020-09-13 22:01:55 +0800

    add rspec for directmsg model

--- spec/factories/directmsgs.rb
+++ spec/factories/directmsgs.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :directmsg do
-    name { "MyString" }
-    deleted_at { "2020-09-10 12:14:49" }
+    association :workspace
+    name { Faker::Name.name }
+    deleted_at { Faker::Date.in_date_period }
   end
 end
--- spec/models/directmsg_spec.rb
+++ spec/models/directmsg_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Directmsg, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe 'association' do
+    it { should have_many(:users_directmsgs) }
+    it { should have_many(:users) }
+    it { should have_many(:messages) }
+    it { should belong_to(:workspace) }
+  end
 end
