befeddbdb8d5b533a63f149f43049c516d07fe9e
hayashi <touyalin@gmail.com>
2020-09-18 21:19:27 +0800

    Change check payload signature flow in github_controller.rb

--- app/controllers/webhook/github_controller.rb
+++ app/controllers/webhook/github_controller.rb
@@ -1,45 +1,42 @@
 class Webhook::GithubController < ActionController::API
 
-  before_action :find_bot_id, :find_channel_id
-
   def payload
-    # TODO: 從資料表撈出 secret
-    github_secrets = ['c9cee6d85787e40ac9ff5384d57ccd4789e4394d', '44b0eb783b2ab91de6b870b9c276a0ed98edcada']
-    # github_secrets = WebhookSecret.all => bad pratice
+    # 測試用的 secrets
+    # github_secrets = [44b0eb783b2ab91de6b870b9c276a0ed98edcada']
 
-    # TODO: (optional): 幫使用者自動產生 secret
+    # TODO: 幫使用者自動產生 secret
     # generate_github_secret = SecureRandom.hex(20) 
 
-    # TODO (optional): rewind 是否必要？
-    # 如果 contenet type 為 application/json 的話似乎才需要 request.body.rewind
-    # request.body.rewind
-    payload_body = request.body.read
-    # byebug
+    # Get repository name from incoming payload
+    json_body = JSON.parse(params[:payload])
+    user_repository_name = json_body["repository"]["full_name"]
     
-    # 驗證 payload 是否為 github 發出
-    github_secrets.each do |github_secret|  
+    # find reposity name form model
+    if check_repo_name_exist?(user_repository_name)
+      # 從資料表撈出 secret
+      github_secret = @search_repo_result.secret
+      
+      # 讀 payload 資料
+      payload_body = request.body.read
+
+      # 驗證 payload
       if signature?(payload_body, github_secret) 
-        # 轉成 json 
-        json_body = JSON.parse(params[:payload]) 
-  
-        # 取得 payload 部份資料
+        # 發送訊息匯整 & 把 hash 轉成 json 格式
         user_event_action_type = json_body["hook"]["events"][0]
-        user_repository_name = json_body["repository"]["name"]
         user_repository_url = json_body["repository"]["html_url"]
-        
-        # TODO: 發送訊息匯整 & 把 hash 轉成 json 格式
         payload_content = { user_event_action_type: user_event_action_type, 
                             user_repository_name: user_repository_name, 
                             user_repository_url: user_repository_url }.to_json
-  
-        # TODO: 發送訊息流程修正
+
+        # 找到 channel id & 發送訊息
+        find_bot_id
+        find_channel_id
         send_webhook_message(@catbot.id, payload_content)
-  
-        render json: { message: 'OK' }, status: :ok   
-        break   
+
+        render json: { message: 'OK' }, status: :ok  
       end
-    end
   end
+end
 
   private
     def signature?(payload_body, github_secret)
@@ -48,12 +45,16 @@ class Webhook::GithubController < ActionController::API
       Rack::Utils.secure_compare(excepted_signature, request.env['HTTP_X_HUB_SIGNATURE'])      
     end
 
+    def check_repo_name_exist?(user_repository_name)
+      @search_repo_result = WebhookRecord.find_by(repo_name: user_repository_name)
+    end
+
     def find_bot_id
       @catbot = User.find_by(nickname: 'CatBot')
     end
 
     def find_channel_id
-      # TODO: 從接收 payload 的路徑找到要發送的 channel_id    
+      # 從接收 payload endpoint 的路徑中，抓到要發送的 channel_id    
       @channel = Channel.find(params[:id])
     end
 
