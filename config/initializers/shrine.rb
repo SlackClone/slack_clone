7656949e8fd1d90a39b46ba30c966fff883871d9
hank <hank8451@gmail.com>
2020-09-25 16:38:51 +0800

    CI corrected

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -3,17 +3,11 @@ require "shrine/storage/file_system"
 require "shrine/storage/memory"
 # require "shrine/storage/s3"
 
-if Rails.env.test? 
-  Shrine.storages = { 
-    cache: Shrine::Storage::memory.new, # temporary 
-    store: Shrine::Storage::memory.new, # permanent 
-  }
-else
-  Shrine.storages = { 
-    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary 
-    store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent 
-  }
-end
+
+Shrine.storages = { 
+  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary 
+  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent 
+
 Shrine.plugin :activerecord 
 Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays 
 Shrine.plugin :restore_cached_data # re-extract metadata when attaching a cached file 
