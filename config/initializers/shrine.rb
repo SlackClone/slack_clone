3628de2c3876f129f906078139355f43ac1a5d37
hank <hank8451@gmail.com>
2020-09-25 16:44:52 +0800

    syntax error correct

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -7,7 +7,7 @@ require "shrine/storage/memory"
 Shrine.storages = { 
   cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary 
   store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent 
-
+}
 Shrine.plugin :activerecord 
 Shrine.plugin :cached_attachment_data # for retaining the cached file across form redisplays 
 Shrine.plugin :restore_cached_data # re-extract metadata when attaching a cached file 
