e63f74aab8df6b5fd017388be633585329144bb2
Leoyang0717 <pithend58639@gmail.com>
2020-10-05 17:03:07 +0800

    rebase success

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,6 @@
 const { environment } = require('@rails/webpacker')
 
+environment.loaders.delete('nodeModules')
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
