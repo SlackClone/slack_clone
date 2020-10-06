f99eb8785a1695347fecdd591fc188a1802dd5f5
Reid <touyalin@gmail.com>
2020-10-06 23:59:38 +0800

    Set timezone to Asia/Taipei in application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module SlackClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'Asia/Taipei'
     # config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
     #   html_tag
     # }
