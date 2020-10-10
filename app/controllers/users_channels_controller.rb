eba23aa88697b689af602a6dce17a634e6c42171
rzchen <jeff820523@gmail.com>
2020-10-10 15:45:53 +0800

    add realtime msg after invite people to channel

--- app/controllers/users_channels_controller.rb
+++ app/controllers/users_channels_controller.rb
@@ -9,10 +9,16 @@ class UsersChannelsController < ApplicationController
   end
 
   def invite
+    # byebug
+    
+
     params["users_channels"]["name"].each do |user|
       @user = User.find_by(email: user)
       @channel.users << @user
-      @channel.save
+      
+      # 把其他人加入 channel 後會即時新增訊息通知
+      invited_message
+      # @channel.save
     end
     redirect_to workspace_channel_path(@workspace,@channel),notice: I18n.t("users_channels.invite",user: params["users_channels"]["name"].join(","))
   end
@@ -25,4 +31,20 @@ class UsersChannelsController < ApplicationController
   def find_workspace
     @workspace = Workspace.find(@channel.workspace_id)
   end
+
+  def invited_message
+    channel_name = @channel.name
+    message = @channel.messages.create(content: "<i>已經被「#{current_user.nickname}」邀請加入 ##{channel_name}</i>", user_id: @user.id)
+    channel_id = @channel.id
+    avatar_url = current_user.profile.try(:avatar_url,(:small))
+    direct_or_not = false
+
+    channel = @channel
+    sender = @user
+
+    SendMessageJob.perform_later(message, channel_id, avatar_url, direct_or_not)
+    SendNotificationJob.perform_later(channel, sender, direct_or_not)
+  end
+
+
 end
