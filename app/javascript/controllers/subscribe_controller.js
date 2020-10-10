80ef47dab7e212f2a7b0d61eb8a860b9334015fd
hank <hank8451@gmail.com>
2020-10-10 21:50:14 +0800

    WIP

--- app/javascript/controllers/subscribe_controller.js
+++ app/javascript/controllers/subscribe_controller.js
@@ -130,6 +130,51 @@ function editor(){
         'imageStyle:side'
       ]
     },
+    mention: {
+      feeds: [
+          {
+              marker: '@',
+              feed: [
+                  { id: '@cflores', avatar: 'm_1', name: 'Charles Flores' },
+                  { id: '@gjackson', avatar: 'm_2', name: 'Gerald Jackson' },
+                  { id: '@wreed', avatar: 'm_3', name: 'Wayne Reed' },
+                  { id: '@lgarcia', avatar: 'm_4', name: 'Louis Garcia' },
+                  { id: '@rwilson', avatar: 'm_5', name: 'Roy Wilson' },
+                  { id: '@mnelson', avatar: 'm_6', name: 'Matthew Nelson' },
+                  { id: '@rwilliams', avatar: 'm_7', name: 'Randy Williams' },
+                  { id: '@ajohnson', avatar: 'm_8', name: 'Albert Johnson' },
+                  { id: '@sroberts', avatar: 'm_9', name: 'Steve Roberts' },
+                  { id: '@kevans', avatar: 'm_10', name: 'Kevin Evans' },
+                  { id: '@mwilson', avatar: 'w_1', name: 'Mildred Wilson' },
+                  { id: '@mnelson', avatar: 'w_2', name: 'Melissa Nelson' },
+                  { id: '@kallen', avatar: 'w_3', name: 'Kathleen Allen' },
+                  { id: '@myoung', avatar: 'w_4', name: 'Mary Young' },
+                  { id: '@arogers', avatar: 'w_5', name: 'Ashley Rogers' },
+                  { id: '@dgriffin', avatar: 'w_6', name: 'Debra Griffin' },
+                  { id: '@dwilliams', avatar: 'w_7', name: 'Denise Williams' },
+                  { id: '@ajames', avatar: 'w_8', name: 'Amy James' },
+                  { id: '@randerson', avatar: 'w_9', name: 'Ruby Anderson' },
+                  { id: '@wlee', avatar: 'w_10', name: 'Wanda Lee' }
+              ],
+              // itemRenderer: customItemRenderer
+          },
+          {
+              marker: '#',
+              feed: [
+                  '#american', '#asian', '#baking', '#breakfast', '#cake', '#caribbean',
+                  '#chinese', '#chocolate', '#cooking', '#dairy', '#delicious', '#delish',
+                  '#dessert', '#desserts', '#dinner', '#eat', '#eating', '#eggs', '#fish',
+                  '#food', '#foodgasm', '#foodie', '#foodporn', '#foods', '#french', '#fresh',
+                  '#fusion', '#glutenfree', '#greek', '#grilling', '#halal', '#homemade',
+                  '#hot', '#hungry', '#icecream', '#indian', '#italian', '#japanese', '#keto',
+                  '#korean', '#lactosefree', '#lunch', '#meat', '#mediterranean', '#mexican',
+                  '#moroccan', '#nom', '#nomnom', '#paleo', '#poultry', '#snack', '#spanish',
+                  '#sugarfree', '#sweet', '#sweettooth', '#tasty', '#thai', '#vegan',
+                  '#vegetarian', '#vietnamese', '#yum', '#yummy'
+              ]
+          }
+      ]
+  },
     licenseKey: '',
     
   } )
