a43b55c431fa323dd6602ffa2aa7be2ec3ed9d6b
Leoyang0717 <pithend58639@gmail.com>
2020-10-12 16:20:06 +0800

    email註冊及邀請格式修改

--- config/initializers/locales.rb
+++ config/initializers/locales.rb
@@ -1,7 +1,7 @@
 class Application < Rails::Application
   config.i18n.load_path += Dir[Rails.root.join('config','locales', '**', '*.yml').to_s]
   config.i18n.available_locales = ["en", "zh-TW"]
-  config.i18n.default_locale = "zh-TW"
+  config.i18n.default_locale = "en"
   # use default locale when translation missing
   config.i18n.fallbacks = true
   
