b1d3e52ab49186ea28beb8cb824d0f1237f39264
hank <hank8451@gmail.com>
2020-10-12 23:38:15 +0800

    mention rspec OK

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -1,5 +1,8 @@
 class Mention < ApplicationRecord
+  validates :name, presence: true
+
   belongs_to :user
+  belongs_to :message
   belongs_to :messageable, :polymorphic => true
   belongs_to :message
 end
