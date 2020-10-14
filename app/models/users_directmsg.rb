d6f02f1e8965022d8a759980e8eaab87a51eeb72
hank <hank8451@gmail.com>
2020-10-14 17:35:20 +0800

    add callback function in usersdirectmsg

--- app/models/users_directmsg.rb
+++ app/models/users_directmsg.rb
@@ -1,4 +1,10 @@
 class UsersDirectmsg < ApplicationRecord
   belongs_to :user
   belongs_to :directmsg
+
+  before_destroy :destroy_directmsg, :abc
+
+  def destroy_directmsg
+    self.directmsg.destroy
+  end
 end
