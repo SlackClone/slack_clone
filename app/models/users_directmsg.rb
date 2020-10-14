7ab119100aa929c5a25169178d26bb8aeb23ae7a
hank <hank8451@gmail.com>
2020-10-14 17:43:46 +0800

    delete redundant syntax

--- app/models/users_directmsg.rb
+++ app/models/users_directmsg.rb
@@ -2,7 +2,7 @@ class UsersDirectmsg < ApplicationRecord
   belongs_to :user
   belongs_to :directmsg
 
-  before_destroy :destroy_directmsg, :abc
+  before_destroy :destroy_directmsg
 
   def destroy_directmsg
     self.directmsg.destroy
