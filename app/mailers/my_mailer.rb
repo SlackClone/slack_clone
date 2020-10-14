b06de24f54c597f9936737ec7f200dc7b3ac7a06
Leoyang0717 <pithend58639@gmail.com>
2020-10-14 15:16:26 +0800

    wip

--- app/mailers/my_mailer.rb
+++ app/mailers/my_mailer.rb
@@ -1,10 +1,24 @@
 class MyMailer < ApplicationMailer
 
   def invite(current_user,workspace,invitation)
-    @invitation = invitation
-    @user = current_user
-    @workspace = workspace
-    mail(to: @invitation.receiver_email, subject: I18n.t("mailer.title",username: @user.nickname))
+    require 'mailgun-ruby'
+    require 'digest/sha2'  
+    
+    def invite(current_user,workspace,invitation)
+      default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@sladock.tw"
+      @invitation = invitation
+      @user = current_user
+      @workspace = workspace
+      # First, instantiate the Mailgun Client with your API key
+      mg_client = Mailgun::Client.new ENV["MAILGUN_API"]
+      # Define your message parameters
+      message_params =  { from: "sladock@sladock.tw",
+                          to:   @invitation.receiver_email,
+                          subject: I18n.t("mailer.title",username: @user.nickname),
+                          html: (render "./my_mailer/invite.html.erb")
+                        }
+      # Send your message through the client
+      mg_client.send_message 'sladock.tw', message_params  
+    end
   end
-  
 end
