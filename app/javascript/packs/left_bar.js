6ef21d4fbd896d0007a5a96e5b04f6f0a75528ba
serenanamee <serena.chang169@gmail.com>
2020-10-15 11:39:48 +0800

    leftbar hamburger

---
+++ app/javascript/packs/left_bar.js
@@ -0,0 +1,16 @@
+
+const menuToggle = document.querySelector('.menu')
+const body = document.querySelector('body')
+const closeMenu = document.querySelector('.close')
+
+menuToggle.addEventListener('click', menuShow )
+closeMenu.addEventListener('click', menuShow)
+
+function menuShow () {
+  body.classList.toggle('toggle')
+}
+
+function menuClose () {
+  closeMenu.classList.remove('toggle')
+  // body.classList.toggle('toggle')
+}
