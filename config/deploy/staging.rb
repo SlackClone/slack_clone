0a23e0e05a514c4c0693e2316d5cdff1777527a7
Reid <touyalin@gmail.com>
2020-10-17 14:41:44 +0800

    Demo env settings

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,12 +3,13 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "staging.sladock.tw", user: "deploy", roles: %w{app db web}, my_property: :my_value
+# server "staging.sladock.tw", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "demo.sladock.tw", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-set :deploy_to, "/home/deploy/staging"
-set :default_env, { path: "/usr/local/rvm/rubies/ruby-2.6.5/bin:$PATH" }
+set :deploy_to, "/home/deploy/demo"
+set :default_env, { path: "/usr/local/ruby-2.6.5/bin:$PATH" }
 
 # role-based syntax
 # ==================
